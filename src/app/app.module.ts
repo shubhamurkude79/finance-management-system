3fa24320d94ce1b54a3d1122b1a13583bf555ceb
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-05-23 19:57:40 +0530

    generated four components and dynamically made it rendered into the app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TransactionSummariesComponent } from './transaction-summaries/transaction-summaries.component';
+import { TransactionFormComponent } from './transaction-form/transaction-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TransactionService } from './services/transaction-service.service';
+import { TransactionListComponent } from './transaction-list/transaction-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TransactionSummariesComponent,
+    TransactionFormComponent,
+    TransactionListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [TransactionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
