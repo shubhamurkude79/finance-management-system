f36964dc053bd24e76d518185331d825b272289f
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-05-29 23:16:03 +0530

    UI aligning done and report chart displaying done ut there is an error

--- src/app/report-chart/report-chart.component.ts
+++ src/app/report-chart/report-chart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Transaction, TransactionService } from '../services/transaction-service.service';
+import * as c3 from 'c3';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-report-chart',
@@ -6,10 +9,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report-chart.component.scss']
 })
 export class ReportChartComponent implements OnInit {
-
-  constructor() { }
+  transactions: Transaction[] = [];
+  subscription: Subscription | undefined;
+  
+  constructor(private transactionService: TransactionService) { }
 
   ngOnInit(): void {
+    this.transactionReportData();
   }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  transactionReportData(): void {
+    this.subscription = this.transactionService.transactions$.subscribe(transactions => {
+      this.transactions = transactions;
+
+      // Transform data for c3.js
+      const dates: [string, ...string[]] = ['x', ...this.transactions.map(transaction => transaction.date.toISOString().split('T')[0])];
+      const amounts: [string, ...number[]] = ['Amount', ...this.transactions.map(transaction => transaction.amount)];
+
+      this.generateBarChart(dates, amounts);
+    });
+  }
+
+  generateBarChart(dates: [string, ...string[]], amounts: [string, ...number[]]): void {
+    c3.generate({
+      bindto: '#expensesChart',
+      data: {
+        x: 'x',
+        columns: [
+          dates,
+          amounts
+        ],
+        type: 'bar'
+      },
+      axis: {
+        x: {
+          type: 'category',
+          tick: {
+            format: '%Y-%m-%d'
+          }
+        },
+        y: {
+          label: {
+            text: 'Transaction Amount',
+            position: 'outer-middle'
+          }
+        }
+      }
+    });
+  }
+
 
 }
