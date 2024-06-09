45e8cd193d2b2cacceb36fe689d3f0fc9c9034b0
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-06-10 00:28:08 +0530

    ngOnDestroy lifecycle hook used to destroy subscription while leaving component

--- src/app/dummy/dummy.component.ts
+++ src/app/dummy/dummy.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -7,12 +8,13 @@ import { environment } from 'src/environments/environment';
   templateUrl: './dummy.component.html',
   styleUrls: ['./dummy.component.scss']
 })
-export class DummyComponent implements OnInit {
+export class DummyComponent implements OnInit, OnDestroy {
   dummyURL = environment.dummyURL;
   dummyData: any[] = [];
   limit: number = 20;
   offset: number = 0;
   allDataLoaded: boolean = false;
+  private subscription: Subscription | undefined;
 
   constructor(private http: HttpClient) { }
 
@@ -20,8 +22,14 @@ export class DummyComponent implements OnInit {
     this.fetchData();
   }
 
+  ngOnDestroy(): void {
+    if(this.subscription){
+      this.subscription?.unsubscribe();
+    }
+  }
+
   fetchData(){
-    this.http.get(`${this.dummyURL}?_limit=${this.limit}&_start=${this.offset}`)
+    this.subscription = this.http.get(`${this.dummyURL}?_limit=${this.limit}&_start=${this.offset}`)
     .subscribe((data: any) => {
       if (data.length < this.limit) {
         this.allDataLoaded = true;
