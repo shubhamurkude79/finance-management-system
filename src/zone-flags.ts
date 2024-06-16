2c2d0dfbae156c43f7c47ff163270e8f3c3525a0
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-06-16 14:37:57 +0530

    polyfills changes that allows fine-grained control over how Zone.js patches browser events, potentially improving performance and debugging experience.

---
+++ src/zone-flags.ts
@@ -0,0 +1,11 @@
+// Disable patching of requestAnimationFrame
+(window as any).__Zone_disable_requestAnimationFrame = true;
+
+// Disable patching of properties like onclick
+(window as any).__Zone_disable_on_property = true;
+
+// Disable patching of specified events
+(window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove'];
+
+// Bypass zone.js patch for IE/Edge
+(window as any).__Zone_enable_cross_context_check = true;
