35ab05d468a8b9a9d39b6680fddc14bd7655d885
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-06-19 23:41:43 +0530

    login and register post methods created adding http client module in  login module file

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 
-export class AuthService {}
+export class AuthService {
+    private authURL = 'dummy-url';
+
+    constructor(private http: HttpClient){}
+
+    login(credentials:any): Observable<any>{
+        return this.http.post(`${this.authURL}/login`, credentials);
+    }
+
+    register(data:any): Observable<any>{
+        return this.http.post(`${this.authURL}/register`, data);
+    }
+}
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginFormComponent } from './login-form/login-form.component';
@@ -13,6 +14,7 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     CommonModule,
+    HttpClientModule,
     LoginRoutingModule
   ]
 })
