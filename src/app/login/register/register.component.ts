1bb3f9989c62bbd7711cded148d8e932aaea3a15
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-06-20 00:25:18 +0530

    reactive registration form created in its component.ts

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm:FormGroup;
+  errorMessage:string = '';
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    if (
+      this.registerForm.valid &&
+      this.registerForm.value.password ===
+        this.registerForm.value.confirmPassword
+    ) {
+      this.authService.register(this.registerForm.value).subscribe(
+        response => {
+          // handle successful registration scenario
+        },
+        error => {
+          this.errorMessage = 'Registration Failed';
+        }
+      )
+    }
   }
 
 }
