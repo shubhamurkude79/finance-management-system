dae645165691a60980223051adf3b70264872362
shubhamurkude79 <shubhamurkude79@gmail.com>
2024-06-19 23:33:39 +0530

    login component removed from module file and authservice file created

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
-import { LoginComponent } from './login.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent,
     LoginFormComponent,
     RegisterComponent
   ],
